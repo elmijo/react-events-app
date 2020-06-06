72395ac6b9ff7c2e236432c80d38b40a77fe27cb
Jerry Anselmi <jerry.anselmi@gmail.com>
2020-06-06 13:56:46 -0300

    Change base structure

---
+++ src/app/componets/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './../../static/css/app.css';
+
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './static/css/index.css';
+import App from './app/componets/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
